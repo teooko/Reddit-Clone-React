cc537b99f3f638c0578ef251648213992570895c
teooko <teobogdann@yahoo.com>
2023-05-23 20:40:41 +0300

    Refactor Footer

---
+++ src/Content/Components/Aside/Footer/Languages.jsx
@@ -0,0 +1,20 @@
+export const Languages = () => {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>English</td>
+                    <td>Deutsch</td>
+                </tr>
+                <tr>
+                    <td>Français</td>
+                    <td>Español</td>
+                </tr>
+                <tr>
+                    <td>Italiano</td>
+                    <td>Português</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
---
+++ src/Content/Components/Aside/Footer/Policy.jsx
@@ -0,0 +1,16 @@
+export const Policy = () => {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <td>User Agreement</td>
+                    <td>Content Policy</td>
+                </tr>
+                <tr>
+                    <td>Privacy Policy</td>
+                    <td>Moderator Code Of Conduct</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
